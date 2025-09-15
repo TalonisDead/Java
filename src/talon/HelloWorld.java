fde92111d4f23af3e6e64ec57ce9aafb075cc3b5
Longtalon <nguyenlong20.ajc@gmail.com>
2025-09-15 08:11:30 +0700

    Hello World

--- src/talon/HelloWorld.java
+++ src/talon/HelloWorld.java
@@ -1,17 +1,18 @@
 package talon;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
-		System.out.println("HelloWorld Talon");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Nhap so: ");
+		int a = scan.nextInt();
+		scan.nextLine();
+		System.out.println("Nhap ten: ");
+		String b = scan.nextLine();
+		System.out.println("Nhap so: ");
+		int c = scan.nextInt();
+
+		System.out.println("ten la: " + b + (" ,tuoi la :" + (a + c)));
 	}
 }
